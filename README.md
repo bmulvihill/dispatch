62329d2dd9cc14bddb8f6d12db54dc051cb2e386
Bryan Mulvihill <mulvihill@informz.com>
2016-12-13 14:28:04 -0500

    update README, move config to Dispatch module

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 # example.cr
 require "./src/dispatch"
 
+Dispatch.configure do |config|
+  config.num_workers = 5
+  config.queue_size = 10
+end
+
 class FakeJob
   include Dispatchable
 
@@ -14,18 +19,22 @@ class FakeJob
   end
 end
 
+Dispatch.config # => <Dispatch::Configuration:0x1042dafb0 @num_workers=5, @queue_size=10>
 FakeJob.dispatch("Bob")
 FakeJob.dispatch("Emily")
 FakeJob.dispatch_in(5.seconds, "Billy")
 FakeJob.dispatch("Maddy")
+Dispatch::SuccessCounter.value # => 0
 
-sleep 10
+sleep 6
+
+Dispatch::SuccessCounter.value # => 4
 ```
 
 Output:
 ```
-"2016-11-30 15:51:04 -0500: Hello, Bob"
-"2016-11-30 15:51:04 -0500: Hello, Emily"
-"2016-11-30 15:51:04 -0500: Hello, Maddy"
-"2016-11-30 15:51:09 -0500: Hello, Billy"
+"2016-12-13 14:23:53 -0500: Hello, Bob"
+"2016-12-13 14:23:53 -0500: Hello, Emily"
+"2016-12-13 14:23:53 -0500: Hello, Maddy"
+"2016-12-13 14:23:58 -0500: Hello, Billy"
 ```
--- spec/configuration_spec.cr
+++ spec/configuration_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 module Dispatch
   describe Configuration do
     Spec.after_each do
-      Dispatcher.configure do |config|
+      Dispatch.configure do |config|
         config.num_workers = 5
         config.queue_size = 100
       end
@@ -11,18 +11,18 @@ module Dispatch
 
     describe "#configure" do
       it "will set the default values" do
-        Dispatch::Dispatcher.config.num_workers.should eq(5)
-        Dispatch::Dispatcher.config.queue_size.should eq(100)
+        Dispatch.config.num_workers.should eq(5)
+        Dispatch.config.queue_size.should eq(100)
       end
 
       it "will allow the setting of configurable values" do
-        Dispatcher.configure do |config|
+        Dispatch.configure do |config|
           config.num_workers = 1234
           config.queue_size = 4321
         end
 
-        Dispatch::Dispatcher.config.num_workers.should eq(1234)
-        Dispatch::Dispatcher.config.queue_size.should eq(4321)
+        Dispatch.config.num_workers.should eq(1234)
+        Dispatch.config.queue_size.should eq(4321)
       end
     end
   end
